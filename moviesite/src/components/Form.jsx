46683dea3ed0e32e8861c6e7fcf7374a36bf25f2
sf23abk <lekanfabiyi@yahoo.com>
2025-07-19 13:45:37 +0100

    a

--- moviesite/src/components/Form.jsx
+++ moviesite/src/components/Form.jsx
@@ -1,25 +1,52 @@
+import React from 'react'
+import AI from "./AI";
 export default  function Form(props){
+      const[formData, setFormdata] = React.useState({question1:"",
+      question2: "", question3: ""
+      })
+      const handleOnChange = (e) =>{
+        const {name,value}  = e.target;
+        setFormdata (prev => ({ ...prev, [name] : value }))
+      }
+      console.log(formData);
     return(
         <>
             {!props.click ? <div className="body">
             <form action="submit" className="form">
                 <div className="t1">
-                    <label htmlFor="question">What’s your favorite movie and why?</label>
-                    <textarea name="question" id="answer"></textarea>
+                    <label htmlFor="answer1">What’s your favorite movie and why?</label>
+                    <textarea 
+                    name="question1" 
+                    id="answer1"
+                    value = {formData.question1}
+                    onChange={handleOnChange}
+                    />
                 </div>
                     <div className="t1">
-                    <label htmlFor="question">Are you in the mood for something new or a classic?</label>
-                    <textarea name="question" id="answer"></textarea>
+                    <label htmlFor="answer2">Are you in the mood for something new or a classic?</label>
+                    <textarea 
+                    name="question2" 
+                    id="answer2"
+                    value = {formData.question2}
+                    onChange={handleOnChange}
+                    />
                 </div>
                     <div className="t1">
-                    <label htmlFor="question">Do you wanna have fun or do you want something serious?</label>
-                    <textarea name="question" id="answer"></textarea>
+                    <label htmlFor="answer3">Do you wanna have fun or do you want something serious?</label>
+                    <textarea 
+                    name="question3" 
+                    id="answer3"
+                    value = {formData.question3}
+                    onChange={handleOnChange}
+                    />
                 </div>
             </form>
           </div>
           
           : <div className="response">
-            
+            <AI
+            data = {formData}
+            />
           </div>
           }
         </>
